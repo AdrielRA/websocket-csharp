6ff7c172c9cd88be9b727d0664467dc55fdf4485
AdrielRA <36385005+AdrielRA@users.noreply.github.com>
2021-07-28 16:13:45 -0300

    :tada: #00001 Commit inicial

---
+++ Websocket.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Websocket
+{
+    partial class Websocket
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Websocket));
+            this.picQR = new System.Windows.Forms.PictureBox();
+            this.picImg = new System.Windows.Forms.PictureBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picQR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQR
+            // 
+            this.picQR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.picQR.Location = new System.Drawing.Point(10, 10);
+            this.picQR.Name = "picQR";
+            this.picQR.Size = new System.Drawing.Size(200, 200);
+            this.picQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picQR.TabIndex = 1;
+            this.picQR.TabStop = false;
+            // 
+            // picImg
+            // 
+            this.picImg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.picImg.Location = new System.Drawing.Point(220, 10);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(350, 350);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImg.TabIndex = 2;
+            this.picImg.TabStop = false;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(10, 218);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(200, 45);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "ATIVAR";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(10, 266);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(200, 45);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "LIMPAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(10, 314);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(200, 45);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "SALVAR";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Websocket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(584, 371);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.picImg);
+            this.Controls.Add(this.picQR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Websocket";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Websocket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picQR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picQR;
+        private System.Windows.Forms.PictureBox picImg;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Websocket.cs
@@ -0,0 +1,121 @@
+﻿using Fleck;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Websocket
+{
+    public partial class Websocket : Form
+    {
+        WebSocketServer server;
+        bool enabled = false;
+
+        public Websocket()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            QRCodeGenerator QG = new QRCodeGenerator();
+            var MyData = QG.CreateQrCode(GetLocalIPAddress(), QRCodeGenerator.ECCLevel.H);
+            var code = new QRCode(MyData);
+            picQR.Image = code.GetGraphic(50);
+
+            btnNew_Click(btnNew, new EventArgs());
+        }
+
+
+        public Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            enabled = !enabled;
+            btnNew.Text = enabled ? "DESATIVAR" : "ATIVAR";
+            btnClear_Click(btnClear, new EventArgs());
+
+            if (!enabled) {
+                if(server != null) server.Dispose();
+                return;
+            }
+            
+            
+            server = new WebSocketServer("ws://0.0.0.0:3000");
+            server.Start(socket =>
+            {
+                //socket.OnOpen = () => Invoke((MethodInvoker)delegate { lblStatus.Text = "Open!\n"; });
+                //socket.OnClose = () => Invoke((MethodInvoker)delegate { lblStatus.Text = "Close!\n"; });
+                socket.OnMessage = message => Invoke((MethodInvoker)delegate {
+                    var payload = message.Split(',')[1];
+                    //lblStatus.Text = payload.Length.ToString();
+                    picImg.Image = Base64ToImage(payload);
+                    btnSave.Enabled = true;
+                    socket.Close();
+                    //server.Dispose();
+                });
+                //socket.OnError = error => Invoke((MethodInvoker)delegate { lblStatus.Text = error.Message; });
+            });
+        }
+
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            picImg.Image = null;
+            picImg.Invalidate();
+            btnSave.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PNG Image(*.png)|*.png|JPG Image(*.jpg)|*.jpg|BMP Image(*.bmp)|*.bmp";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string ext = Path.GetExtension(sfd.FileName).ToLower();
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                picImg.Image.Save(sfd.FileName, format);
+            }
+        }
+    }
+}
